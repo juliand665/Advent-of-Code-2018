1a18c1d7aaa1454b078976c3ff9c1cdb2543ae5b
Julian Dunskus <julian.dunskus@gmail.com>
2021-12-27 20:44:22 +0100

    day 23 part 2 attempt
    this is from ages ago, i'm just saving it

--- Advent Of Code 2018/Day 23/main.swift
+++ Advent Of Code 2018/Day 23/main.swift
@@ -39,4 +39,86 @@ extension Nanobot: Parseable {
 let bots = input().lines().map(Nanobot.init)
 let strongestBot = bots.max { $0.range < $1.range }!
 let botsInRange = bots.count { $0.position.distance(to: strongestBot.position) <= strongestBot.range }
-print(botsInRange)
+print("bots in range:", botsInRange)
+
+// MARK: -
+
+struct Endpoint {
+	var position: Int
+	var kind: Kind
+	
+	enum Kind {
+		case lower, upper
+	}
+}
+
+func rangeEndpoints(in component: (Nanobot) -> Int) -> [Endpoint] {
+	[]
+		+ bots.map { Endpoint(position: component($0) - $0.range, kind: .lower) }
+		+ bots.map { Endpoint(position: component($0) + $0.range, kind: .upper) }
+}
+
+struct Overlap {
+	var range: ClosedRange<Int>
+	var count: Int
+	
+	func distance(to point: Int) -> Int {
+		if range.contains(point) {
+			return 0
+		} else {
+			return min(
+				abs(point - range.lowerBound),
+				abs(point - range.upperBound)
+			)
+		}
+	}
+}
+
+func sortedOverlaps(in component: (Nanobot) -> Int) -> [Overlap] {
+	let endpoints = rangeEndpoints(in: component).sorted(on: ^\.position)
+	
+	var overlaps: [Overlap] = []
+	var level = 0
+	for (start, end) in zip(endpoints, endpoints.dropFirst()) {
+		switch start.kind {
+		case .lower:
+			level += 1
+		case .upper:
+			level -= 1
+		}
+		
+		overlaps.append(Overlap(range: start.position...end.position, count: level))
+	}
+	assert(level == 1)
+	
+	return overlaps
+		.sorted { $0.count }
+		.reversed() // best first
+}
+
+func distanceToClosestPosition(in overlaps: [Overlap]) -> Int {
+	return overlaps
+		.map { $0.distance(to: 0) }
+		.min()!
+}
+
+// this componentwise approach doesn't work because distance to a nanobot (and thus whether or not you're in range) depends on all 3 dimensions…
+let components = [\Nanobot.position.x, \.position.y, \.position.z]
+let overlapsByComponent = components
+	.map(^)
+	.map(sortedOverlaps(in:))
+
+let targetCount = overlapsByComponent
+	.map { $0.first!.count }
+	.min()!
+
+print("best possible count:", targetCount)
+
+let distance = overlapsByComponent
+	.map({ $0
+		.filter { $0.count >= targetCount }
+		.map { $0.distance(to: 0) }
+		.min()!
+	})
+	.reduce(0, +)
+print("distance to closest optimal point:", distance)
